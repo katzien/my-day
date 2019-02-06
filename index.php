b5262adc829ebf11016b761d36ad3b059736c71e
Kat <kasia.zien@gmail.com>
2019-02-06 14:44:20 +0000

    My day

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+
+$myDay = new MyDay();
+//$myDay->run();
+
+for ($i = 0; $i < 5; $i++) {
+    $myDay->run();
+}
+
+class MyDay
+{
+    public function run()
+    {
+        $this->getReady();
+        $this->work();
+        $this->enjoyEvening();
+        $this->windDown();
+    }
+
+    private function getReady()
+    {
+        echo '🌅 Getting ready:' . PHP_EOL;
+        $this->wakeUp();
+        $this->shower();
+        $this->getDressed();
+    }
+
+    private function work()
+    {
+        echo '🤓 Work:' . PHP_EOL;
+        if (rand(0, 4) % 2 == 0) {
+            $this->walkBingo();
+        }
+    }
+
+    private function enjoyEvening()
+    {
+        echo '💃 Evening:' . PHP_EOL;
+        switch (rand(0, 2)) {
+            case 0:
+                $this->sport();
+                break;
+            case 1:
+                $this->netflixNChill();
+                break;
+            case 2:
+                $this->friends();
+                break;
+        }
+    }
+
+    private function windDown()
+    {
+        echo '😴 Wind down:' . PHP_EOL;
+
+        if (rand(0, 1) == 1) {
+            $this->read();
+        }
+
+        $this->sleep();
+    }
+
+    private function wakeUp()
+    {
+        echo '🛏 wake up' . PHP_EOL;
+    }
+
+    private function shower()
+    {
+        echo '🚿 shower' . PHP_EOL;
+    }
+
+    private function getDressed()
+    {
+        echo '👚 get dressed' . PHP_EOL;
+    }
+
+    private function walkBingo()
+    {
+        echo '🐕 walk Bingo' . PHP_EOL;
+    }
+
+    private function sport()
+    {
+        echo '👟 run' . PHP_EOL;
+    }
+
+    private function netflixNChill()
+    {
+        echo '🍿 TV' . PHP_EOL;
+    }
+
+    private function friends()
+    {
+        echo '🍻 friends' . PHP_EOL;
+    }
+
+    private function read()
+    {
+        echo '📚 books' . PHP_EOL;
+    }
+
+    private function sleep()
+    {
+        echo '💤 sleep' . PHP_EOL;
+    }
+}
